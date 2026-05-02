7be888cbde169160ee8750ff30f3e155efa20367
igor-gorohovsky <igor.gorohhovsky@yandex.ru>
2026-05-02 12:55:23 +0300

    refactor: remove unused plugin entrypoint

--- plugin/agent_review.lua
+++
@@ -1,8 +0,0 @@
-if vim.g.loaded_agent_review_nvim == 1 then
-  return
-end
-vim.g.loaded_agent_review_nvim = 1
-
--- Intentionally no side effects here.
--- Call require("agent_review").setup() from your config to define commands,
--- install keymaps/autocmds, and start the optional Neovim socket server.
